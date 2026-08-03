2d241c985330a44f395f26eb7ed096708abf2216
DashawnMitchell <mitchelldashawn@gmail.com>
2026-08-03 18:09:32 -0400

    wrapped up GameInfoPage

--- Pages/GameInfoPage.js
+++ Pages/GameInfoPage.js
@@ -4,18 +4,29 @@ class GameInfoPage {
         this.page = page;
         this.getGameName = this.page.getByRole('heading', {level:1});
         this.getPricesGameContentTab = this.page.getByRole('link', {name:'Prices'});
+        this.getTotalPricedGameStoreFronts = this.page.locator('[class^="cell cell--shop"]');
         this.getHistoryGameContentTab = this.page.getByRole('link', {name:'History'});
         this.getStatsGameContentTab = this.page.getByRole('link', {name:'Stats'});
         this.getRegionsGameContentTab = this.page.getByRole('link', {name:'Regions'});
-        this.getAllGamePageDataCharts = this.page.locator('[data-highcharts-chart]');
+        this.getTotalGamePageDataCharts = this.page.locator('[data-highcharts-chart]');
         this.getAllGamePageLogTableItems = this.page.locator('[data-log-shop]');
         this.getHistoryTabPriceOverview = this.page.getByRole('button').filter({has: page.locator('span'), hasText: 'Price Overview'}); 
         this.getHistoryTabPriceDetail = this.page.getByRole('button').filter({has: page.locator('span'), hasText: 'Price Detail'});
         this.getHistoryTabCutOverview = this.page.getByRole('button').filter({has: page.locator('span'), hasText: 'Cut Overview'});
+        this.getStatsTabPlayersHeadLine = this.page.getByRole('heading', {level:2}).filter({hasText: 'Players on Steam'});
+        this.getStatsTabCollectionHeadLine = this.page.getByRole('heading', {level:2}).filter({hasText: 'Waitlist and Collection Trends'});
+        this.getStatsTabPriceLimitHeadLine = this.page.getByRole('heading', {level:2}).filter({hasText: 'Waitlist Price Limit Distribution'});
+        this.getStatsTabCutLimitHeadLine = this.page.getByRole('heading', {level:2}).filter({hasText: 'Waitlist Cut Limit Distribution'});
+        this.getTotalRegionDisplayed = this.page.locator('[class^="cell cell--shop"]');
     }
 
     async switchGameInfoToPrices(){
         await this.getPricesGameContentTab.click();
+    }
+
+    async getTotalStoreGameIsSold(){
+        let totalOfStores = await this.getTotalPricedGameStoreFronts.count();
+        return totalOfStores;
     }
 
     async switchGameInfoToHistory(){
@@ -42,11 +53,21 @@ class GameInfoPage {
     }
 
     async switchGameInfoToStats(){
-        
+        await this.getStatsGameContentTab.click(); 
+    }
+
+    async getTotalMainCharts(){
+        let allChartsOnPgMainSection = await this.getTotalGamePageDataCharts.count();
+        return allChartsOnPgMainSection;
     }
 
     async switchGameInfoToRegions(){
-        
+        await this.getRegionsGameContentTab.click();
+    }
+
+    async getTotalRegionGameIsSold(){
+        let totalOfRegion = await this.getTotalRegionDisplayed.count();
+        return totalOfRegion;
     }
 
 }
--- tests/gameinfopage.spec.ts
+++ tests/gameinfopage.spec.ts
@@ -6,28 +6,20 @@ test.beforeEach(async ({page}) => {
     console.log(`Running ${test.info().title}`);
     const homePage = new Homepage( page );
     await homePage.goto();
-
     homePage.navigateToTopGame();
     await expect(page.getByText('Prices')).toHaveText('Prices');
     //await page.screenshot({ path: `screenshot/TopGamePage.png` });
 });
 
 test('Displaying a Games Price chart information', async ({page}) => {
-
     const gamePage = new GameInfoPage(page);
     gamePage.switchGameInfoToPrices();
     // await expect(page.getByText(' Prices', {exact: true})).toBeVisible();
     await expect(page.locator('[data-anchor="prices"]')).toBeVisible();
     await page.screenshot({ path: `screenshot/generalGameInfoPage.png` });
-
-//data-anchor="prices"
-//data-highcharts-chart - will get the data charts
-//[class^="selected "] -> will get the currently selected chart
-//[data-log-shop] -> will get the data itmes from Log table
-////a[contains(text(), "Log In")]
 });
 
-test('Switching to the Histoty Tab and confirming exisitence of data tables', async ({page}) => {
+test('Switching to the Histoty Tab and confirming exisitence of various tables by switching to them', async ({page}) => {
     const gamePage = new GameInfoPage(page);
     gamePage.switchGameInfoToHistory();
     await expect( gamePage.getHistoryTabPriceOverview ).toContainClass('selected');
@@ -35,4 +27,25 @@ test('Switching to the Histoty Tab and confirming exisitence of data tables', as
     await expect( gamePage.getHistoryTabPriceDetail ).toContainClass('selected');
     gamePage.selectGameHistoryPriceChart('Cut Overview');
     await expect( gamePage.getHistoryTabCutOverview ).toContainClass('selected');
+    await page.screenshot({ path: `screenshot/GameInfoHistoryPage.png` });
+});
+
+test('Switching to the Stats Tab and confirming expected headers and data charts', async ({page}) => {
+    const gamePage = new GameInfoPage(page);
+    gamePage.switchGameInfoToStats();
+    await expect( gamePage.getStatsTabPlayersHeadLine).toBeVisible();
+    await expect( gamePage.getStatsTabCollectionHeadLine).toBeVisible();
+    await expect( gamePage.getStatsTabPriceLimitHeadLine).toBeVisible();
+    await expect( gamePage.getStatsTabCutLimitHeadLine).toBeVisible();
+    await expect( gamePage.getTotalMainCharts()).toBeGreaterThan(1);
+    await page.screenshot({ path: `screenshot/GameInfoStatsPage.png` });
+});
+
+test('Switching to the Regions Tab and confirming at least the minimal of regions matches the total of stoes the game is sold', async ({page}) => {
+    const gamePage = new GameInfoPage(page);
+    const storeTotal = await gamePage.getTotalStoreGameIsSold();
+    gamePage.switchGameInfoToRegions();
+    await expect( page.getByRole('heading', {level:2}).filter({hasText: 'Steam'}) ).toBeVisible();
+    await expect( await gamePage.getTotalRegionGameIsSold()).toBeGreaterThanOrEqual(storeTotal);
+    await page.screenshot({ path: `screenshot/GameInfoRegionsPage.png` });
 });
